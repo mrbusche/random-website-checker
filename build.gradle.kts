433e494fbe94a3394d623190e80fbec0aa8c7bca
Matt Busche <mrbusche@gmail.com>
2021-01-21 21:03:34 -0600

    latest spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.2"
+    id("org.springframework.boot") version "2.5.0-M1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.sonarqube") version "3.1"
     id("org.jetbrains.kotlin.jvm") version "1.4.21-2"
@@ -14,8 +14,9 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
-    //maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/milestone") }
 }
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -24,12 +25,14 @@ dependencies {
     implementation("com.google.code.gson:gson")
     implementation("org.jsoup:jsoup:1.13.1")
 }
+
 tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
         exceptionFormat = FULL
     }
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
