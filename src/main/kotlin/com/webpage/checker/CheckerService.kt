209504e97783d76441ee4a6ed583d12ffb84d70b
Matt Busche <mrbusche@gmail.com>
2021-07-10 17:24:40 -0500

    null check

--- src/main/kotlin/com/webpage/checker/CheckerService.kt
+++ src/main/kotlin/com/webpage/checker/CheckerService.kt
@@ -28,7 +28,7 @@ class CheckerService {
 
         fun retrieveDynastyDate(url: String): String {
             val document = Jsoup.connect(url).get()
-            return document.select("span[data-dateformat]").first().childNode(0).toString()
+            return document.select("span[data-dateformat]")?.first()?.childNode(0)?.toString()
         }
 
         fun retrieveETRDynastyDate(url: String): String {
@@ -38,7 +38,7 @@ class CheckerService {
 
         fun retrieveSoftwareVersions(url: String, software: String): String {
             val document = Jsoup.connect(url).get()
-            val childNodes = document.select("tbody").first().childNodes()
+            val childNodes = document.select("tbody")?.first()?.childNodes()
             for (childNode in childNodes) {
                 if (childNode is Element && childNode.childNode(1).toString().contains(software)) {
                     return childNode.childNode(3).childNode(0).toString()
