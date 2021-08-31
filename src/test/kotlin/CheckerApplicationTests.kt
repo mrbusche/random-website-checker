ea1bb5d593c2f23c569ef911d5042049dc7e1d31
Matt Busche <mrbusche@gmail.com>
2021-08-30 21:57:10 -0500

    Update CheckerApplicationTests.kt

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -15,7 +15,7 @@ internal class CheckerApplicationTests {
     @Test
     fun `Test ESPN Dynasty Ranks Update`() {
         val dynastyUrl = "https://www.espn.com/fantasy/football/story/_/id/15698900/mike-clay-top-240-dynasty-fantasy-football-rankings-nfl"
-        assertEquals("Aug 24, 2021", CheckerService.retrieveDynastyDate(dynastyUrl))
+        assertEquals("Aug 29, 2021", CheckerService.retrieveDynastyDate(dynastyUrl))
     }
 
     @Test
