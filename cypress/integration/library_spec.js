e65ddcd84aeff468e44ea6bb2327c71b629562a5
Matt Busche <mrbusche@gmail.com>
2021-09-03 22:27:00 -0500

    check library

---
+++ cypress/integration/library_spec.js
@@ -0,0 +1,28 @@
+describe('check kirkendall', () => {
+    it('checks for new releases', () => {
+        cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
+        cy.get('.c-results-utility-result-count').contains('of 37');
+    })
+
+    it('checks new books', () => {
+        cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
+        cy.get('.new-releases__link').first().contains('The King\'s fifth');
+    })
+
+    it('checks new videos', () => {
+        cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
+        cy.get('.new-releases__link').first().contains('Raging Bull');
+    })
+})
+
+describe('check bridges audio', () => {
+    it('checks for new audiobooks', () => {
+        cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
+        cy.get('.search-text').contains('of 1 results');
+    })
+
+    // it('checks for new ebooks', () => {
+    //     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
+    //     cy.get('.search-text').contains('of 7 results');
+    // })
+})
