5b7628e57499ebcd172712d8cf14c8802a7d6d7f
Matt Busche <mrbusche@gmail.com>
2021-09-14 08:23:31 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -6,12 +6,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link').first().contains('The warrant');
+        cy.get('.new-releases__link').first().contains('Don\'t swap your sweater for a dog');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link').first().contains('PAW Patrol');
+        cy.get('.new-releases__link').first().contains('The warrant');
     })
 })
 
