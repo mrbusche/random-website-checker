3a553749873dd92af7f976a6ec4c2cf9ef27f658
Matt Busche <mrbusche@gmail.com>
2021-09-15 16:41:32 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -1,7 +1,7 @@
 describe('check kirkendall', () => {
     it('checks for new releases', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
-        cy.get('.c-results-utility-result-count').contains('of 54');
+        cy.get('.c-results-utility-result-count').contains('of 52');
     })
 
     it('checks new books', () => {
@@ -20,7 +20,7 @@ describe('check bridges audio', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text').contains('of 14 results');
+                cy.get('.search-text').contains('of 42 results');
             }
         });
     })
