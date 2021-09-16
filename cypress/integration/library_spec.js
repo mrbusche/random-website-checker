d9c1cfb8adda56ed199cd4a44bb7864759d92183
Matt Busche <mrbusche@gmail.com>
2021-09-16 13:21:06 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -28,7 +28,7 @@ describe('check bridges audio', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text').contains('of 24 results');
+                cy.get('.search-text').contains('of 30 results');
             }
         });
     })
