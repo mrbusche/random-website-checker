44bf74739733bc57d2e8c9130f7f8fdc10595f15
Matt Busche <mrbusche@gmail.com>
2021-09-22 08:05:02 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -6,12 +6,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link').first().contains('Nikki');
+        cy.get('.new-releases__link').first().contains('The theater of war : what ancient Greek tragedies can teach us today');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link').first().contains('The Boss Baby : Family Business');
+        cy.get('.new-releases__link').first().contains('The evil next door');
     })
 })
 
