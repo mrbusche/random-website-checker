dfac3a0a7339498a5ec8538b835f25686ab1feec
Matt Busche <mrbusche@gmail.com>
2021-09-22 16:54:49 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -1,7 +1,7 @@
 describe('check kirkendall', () => {
     it('checks for new releases', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
-        cy.get('.c-results-utility-result-count').contains('of 56');
+        cy.get('.c-results-utility-result-count').contains('of 53');
     })
 
     it('checks new books', () => {
@@ -15,7 +15,7 @@ describe('check kirkendall', () => {
     })
 })
 
-describe('check bridges audio', () => {
+describe('check bridges', () => {
     it('checks for new audiobooks', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
@@ -28,7 +28,7 @@ describe('check bridges audio', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text').contains('of 90 results');
+                cy.get('.search-text').contains('of 152 results');
             }
         });
     })
