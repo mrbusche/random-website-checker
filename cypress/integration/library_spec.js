df0598af07e5b452e5fab4a513f7e5c97b007575
Matt Busche <mrbusche@gmail.com>
2021-09-24 06:59:04 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -1,17 +1,23 @@
 describe('check kirkendall', () => {
     it('checks for new releases', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
-        cy.get('.c-results-utility-result-count').contains('of 53');
+        cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
+            const text = $div.text();
+            expect(text).to.eq('1-10 of 49');    
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link').first().contains('finding truth in the chaos of a relativistic world');
+        cy.get('.new-releases__link').first().should(($div) => {
+            const text = $div.text();
+            expect(text).to.eq('56 days');    
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link').first().contains('Cruella');
+        cy.get('.new-releases__link').first().should(($div) => {
+            const text = $div.text();
+            expect(text).to.eq('Candyman (1992)');    
     })
 })
 
