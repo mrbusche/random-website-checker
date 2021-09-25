2b36b5e1e1e2f86b71fd8dd0c507183f8817142a
Matt Busche <mrbusche@gmail.com>
2021-09-25 09:27:47 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', '56 days');
+        cy.get('.new-releases__link a').first().should('have.text', 'Smoke');
     })
 
     it('checks new videos', () => {
