c67f7e21426a409bbb310bafdd81c9e68e968379
Matt Busche <mrbusche@gmail.com>
2021-10-02 08:36:42 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'We march at midnight : a war memoir');
+        cy.get('.new-releases__link a').first().should('have.text', 'Blood like magic');
     })
 
     it('checks new videos', () => {
