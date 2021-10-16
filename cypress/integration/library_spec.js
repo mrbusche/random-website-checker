ec9963fcec75ddfd31e8fecf4e6ae8d84da78822
Matt Busche <mrbusche@gmail.com>
2021-10-16 09:03:36 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Basic life support (BLS) provider manual : a comprehensive guide covering the latest guidelines);
+        cy.get('.new-releases__link a').first().should('have.text', 'Basic life support (BLS) provider manual : a comprehensive guide covering the latest guidelines');
     })
 
     it('checks new videos', () => {
