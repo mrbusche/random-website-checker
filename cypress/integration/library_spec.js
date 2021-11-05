099565317f6510369749e5f8b4d4586acf0c8602
Matt Busche <mrbusche@gmail.com>
2021-11-05 10:15:56 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Van Gogh and the artists he loved');
+        cy.get('.new-releases__link a').first().should('have.text', 'Best in snow');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Thomas & friends. Blue Mountain mystery : the movie');
+        cy.get('.new-releases__link a').first().should('have.text', 'Kung Fu: The complete first season');
     })
 })
 
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '59 results');
+                cy.get('.search-text.h2').first().should('contain.text', '60 results');
             }
         });
     })
