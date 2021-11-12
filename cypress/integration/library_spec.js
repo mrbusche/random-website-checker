66b72e5faae8de9206e4f428285c5ad6f4a07dd8
Matt Busche <mrbusche@gmail.com>
2021-11-12 10:15:59 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Sistersong');
+        cy.get('.new-releases__link a').first().should('have.text', 'Salt magic');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The crown. Season 4');
+        cy.get('.new-releases__link a').first().should('have.text', 'PAW Patrol : The Movie');
     })
 })
 
