b595fb7345b58ccda3b99c5c3de5fde80c3fe131
Matt Busche <mrbusche@gmail.com>
2021-12-02 06:52:46 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Someone perfect');
+        cy.get('.new-releases__link a').first().should('have.text', 'The shadows of men : a novel');
     })
 
     it('checks new videos', () => {
