1831e96667b476118874fb1f84af6f1a14b456dd
Matt Busche <mrbusche@gmail.com>
2021-12-03 06:57:28 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The shadows of men : a novel');
+        cy.get('.new-releases__link a').first().should('have.text', 'King of battle and blood');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Shang-Chi and the legend of the ten rings');
+        cy.get('.new-releases__link a').first().should('have.text', 'Our Ladies');
     })
 })
 
