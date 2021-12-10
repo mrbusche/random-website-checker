108f6e2dbe75c6a4d44f66508f5327442ec6644d
Matt Busche <mrbusche@gmail.com>
2021-12-10 09:55:18 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Dream Street');
+        cy.get('.new-releases__link a').first().should('have.text', 'Flying blind : the 737 MAX tragedy and the fall of Boeing');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Cry Macho');
+        cy.get('.new-releases__link a').first().should('have.text', 'The Vikings');
     })
 })
 
