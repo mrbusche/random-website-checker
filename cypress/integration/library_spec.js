c9bdd7dd1eeebd5b38027db44a3702f5b046059e
Matt Busche <mrbusche@gmail.com>
2021-12-17 08:18:14 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Me without you');
+        cy.get('.new-releases__link a').first().should('have.text', 'My life with blindness');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The last duel');
+        cy.get('.new-releases__link a').first().should('have.text', 'Rick and Morty. Season 5');
     })
 })
 
