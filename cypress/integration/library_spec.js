f99ae00f79363b1686275eeeea8ac42226fe26d2
Matt Busche <mrbusche@gmail.com>
2022-01-04 08:17:27 -0600

    Update library_spec.js (#289)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -14,7 +14,7 @@ describe('check kirkendall', () => {
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Brokenwood mysteries. Series 7');
+        cy.get('.new-releases__link a').first().should('have.text', 'The French Dispatch');
     })
 })
 
