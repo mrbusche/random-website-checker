7697920068011c0ebff5db63740d940bfdef1d7a
Matt Busche <mrbusche@gmail.com>
2022-01-06 07:20:48 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The secret lives of customers : a detective story about solving the mystery of customer behavior');
+        cy.get('.new-releases__link a').first().should('have.text', 'Build it! : make supercool models with your favorite LEGO parts. Christmas');
     })
 
     it('checks new videos', () => {
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '146 results');
+                cy.get('.search-text.h2').first().should('contain.text', '122 results');
             }
         });
     })
