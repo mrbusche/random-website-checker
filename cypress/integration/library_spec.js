83d270c141356e8ea8b673dc6d47a98e6b2be6f8
Matt Busche <mrbusche@gmail.com>
2022-01-19 08:27:07 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'End of days : a Pike Logan novel');
+        cy.get('.new-releases__link a').first().should('have.text', 'The school for good mothers : a novel');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Dry');
+        cy.get('.new-releases__link a').first().should('have.text', 'Brian Wilson : Long Promised Road');
     })
 })
 
