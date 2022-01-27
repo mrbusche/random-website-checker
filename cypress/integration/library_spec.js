f987e3f34f253a700508ee1bee6949370d9e4637
Matt Busche <mrbusche@gmail.com>
2022-01-27 07:33:02 -0600

    Update library_spec.js (#326)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Llama Llama I love you');
+        cy.get('.new-releases__link a').first().should('have.text', 'The cobbler of Spanish Fort and other frontier stories');
     })
 
     it('checks new videos', () => {
