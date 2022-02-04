3c54c389fb331682be9de25c90e7b20e46a6c4cb
Matt Busche <mrbusche@gmail.com>
2022-02-04 07:30:30 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Do You Follow? : A Thriller');
+        cy.get('.new-releases__link a').first().should('have.text', 'Ramses the damned : the reign of Osiris');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Ghostbusters : afterlife');
+        cy.get('.new-releases__link a').first().should('have.text', 'Fortress');
     })
 })
 
