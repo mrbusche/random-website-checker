b626ae344fbc733d8f33984c948d5a4f38c01bf7
Matt Busche <mrbusche@gmail.com>
2022-02-17 08:10:48 -0600

    Update library_spec.js (#355)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Eyes that speak to the stars');
+        cy.get('.new-releases__link a').first().should('have.text', 'Walking the bowl : a true story of murder and survival among the street children of Lusaka');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Resident evil: Welcome to Raccoon City');
+        cy.get('.new-releases__link a').first().should('have.text', 'Stargirl. The complete second season.');
     })
 })
 
