ffb848da6a1c8add01f9307913c59a4960607cff
Matt Busche <mrbusche@gmail.com>
2022-02-23 08:26:27 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Watch my smoke');
+        cy.get('.new-releases__link a').first().should('have.text', 'Between two trees : our transformation from death to life');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Eternals');
+        cy.get('.new-releases__link a').first().should('have.text', '8Shattered');
     })
 })
 
