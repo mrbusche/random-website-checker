3f5cbc4e6b62e51deb6ba2ffa63fe42cf383070d
Matt Busche <mrbusche@gmail.com>
2022-03-09 20:24:06 -0600

    Update library_spec.js (#384)
    * Update library_spec.js
    
    * Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Grimrose girls');
+        cy.get('.new-releases__link a').first().should('have.text', 'A tale of sorcery...');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Dukes of Hazzard. The complete fourth season');
+        cy.get('.new-releases__link a').first().should('have.text', 'The nowhere inn');
     })
 })
 
