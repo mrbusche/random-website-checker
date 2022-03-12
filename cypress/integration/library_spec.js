b47ac0bc28943f87fffff43f5977a9b983ca2624
Matt Busche <mrbusche@gmail.com>
2022-03-12 10:09:58 -0600

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Crushing the red flowers');
+        cy.get('.new-releases__link a').first().should('have.text', 'Red paint : an ancestral autobiography');
     })
 
     it('checks new videos', () => {
