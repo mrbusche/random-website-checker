11860428231f8c6d5188359f0574be128b76b0e4
Matt Busche <mrbusche@gmail.com>
2022-03-23 08:14:59 -0500

    Update library_spec.js (#401)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Discover her art : women artists and their masterpieces');
+        cy.get('.new-releases__link a').first().should('have.text', 'The bug robot');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Science Kids : Covid 19. Understanding breakthrough infections.');
+        cy.get('.new-releases__link a').first().should('have.text', 'Nightmare alley');
     })
 })
 
