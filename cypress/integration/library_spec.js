b8f0b40d5463fa1131c8b6cff8cc9b894bb6cb89
Matt Busche <mrbusche@gmail.com>
2022-03-26 17:07:42 -0500

    Update library_spec.js (#407)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Fallen creed');
+        cy.get('.new-releases__link a').first().should('have.text', 'New naturalism : designing and planting a resilient, ecologically vibrant home garden');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '20 results');
+                cy.get('.search-text.h2').first().should('contain.text', '8 results');
             }
         });
     })
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '50 results');
+                cy.get('.search-text.h2').first().should('contain.text', '49 results');
             }
         });
     })
