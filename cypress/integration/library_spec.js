0638e99d55c3bb0648b01f50e4938d95823b0aed
Matt Busche <mrbusche@gmail.com>
2022-03-29 08:05:10 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'New naturalism : designing and planting a resilient, ecologically vibrant home garden');
+        cy.get('.new-releases__link a').first().should('have.text', 'All that\'s left in the world');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Nightmare alley');
+        cy.get('.new-releases__link a').first().should('have.text', 'Dexter : new blood, Season 1');
     })
 })
 
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '50 results');
+                cy.get('.search-text.h2').first().should('contain.text', '11 results');
             }
         });
     })
