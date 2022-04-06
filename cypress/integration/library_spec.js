1e8aaede45418ea786b5a35beba4dcb4ad8cdeb5
Matt Busche <mrbusche@gmail.com>
2022-04-06 12:47:58 -0500

    Update library_spec.js (#424)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Willa the silver glitter dragon');
+        cy.get('.new-releases__link a').first().should('have.text', 'Four treasures of the sky');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '21 results');
+                cy.get('.search-text.h2').first().should('contain.text', '119 results');
             }
         });
     })
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '159 results');
+                cy.get('.search-text.h2').first().should('contain.text', '113 results');
             }
         });
     })
