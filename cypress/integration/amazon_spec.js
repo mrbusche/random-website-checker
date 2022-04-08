f6b39f40caaa8cc1db6ef7d97063235ee2a68eea
Matt Busche <mrbusche@gmail.com>
2022-04-07 21:56:03 -0500

    add ebook checker

---
+++ cypress/integration/amazon_spec.js
@@ -0,0 +1,16 @@
+describe('check ebook prices', () => {
+    it('check for ebooks on sale', () => {
+        cy.visit('https://smile.amazon.com/hz/wishlist/genericItemsPage/153OV2P85MJD6?type=wishlist&filter=unpurchased&sort=price-asc&viewType=list');
+        // cy.get('.itemPriceDrop').parent().should('have.text', 'hello');
+        cy.get('.itemPriceDrop').first().parent().should(($div) => {
+            const text = $div.text().replace(/\s\s+/g, ' ').trim();
+            if (text) {
+                const amt = text.substring(text.indexOf('$') + 1);
+                if (amt) {
+                    const val = parseFloat(amt.substring(0, 4), 10) > 5;
+                    expect(val).to.be.true;
+                }
+            }
+        });
+    })
+})
