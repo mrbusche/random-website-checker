b95f8d32ad492120cdcd9e1a1f397d21815f3d95
Matt Busche <mrbusche@gmail.com>
2022-04-07 21:26:04 -0500

    Update dynasty_spec.js

--- cypress/integration/dynasty_spec.js
+++ cypress/integration/dynasty_spec.js
@@ -1,11 +1,11 @@
 describe('check dynasty things', () => {
     it('checks ESPN Dynasty updates', () => {
         cy.visit('https://www.espn.com/fantasy/football/story/_/id/15698900/mike-clay-top-240-dynasty-fantasy-football-rankings-nfl');
-        cy.get('[data-dateformat="date1"]').first().should('have.text', 'Nov 19, 2021');
+        cy.get('[data-dateformat="date1"]').first().should('have.text', 'Mar 9, 2022');
     })
 
     it('checks ETR Dynasty updates', () => {
         cy.visit('https://establishtherun.com/etrs-dynasty-rankings/');
-        cy.get('span.published').first().should('have.text', 'Oct 20, 2021');
+        cy.get('span.published').first().should('have.text', 'MAR 20, 2022');
     })
 })
