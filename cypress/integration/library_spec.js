77a248576e4293ded5f8bc1c855f518fdd8b2856
Matt Busche <mrbusche@gmail.com>
2022-04-09 16:50:50 -0500

    Update library_spec.js (#430)

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '185 results');
+                cy.get('.search-text.h2').first().should('contain.text', '208 results');
             }
         });
     })
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '191 results');
+                cy.get('.search-text.h2').first().should('contain.text', '211 results');
             }
         });
     })
