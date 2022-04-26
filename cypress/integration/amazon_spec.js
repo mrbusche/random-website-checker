0ef9d8b364d1808d485eec768b87f3eccdec9f87
Matt Busche <mrbusche@gmail.com>
2022-04-25 20:19:44 -0500

    Update amazon_spec.js

--- cypress/integration/amazon_spec.js
+++ cypress/integration/amazon_spec.js
@@ -1,20 +1,18 @@
-describe('check ebook prices', () => {
-    it('check for ebooks on sale', () => {
-        cy.visit('https://smile.amazon.com/hz/wishlist/genericItemsPage/153OV2P85MJD6?type=wishlist&filter=unpurchased&sort=price-asc&viewType=list');
-        
-        cy.get('body').then((body) => {  
-            if (body.find('.itemPriceDrop').length > 0) {
-                cy.get().first().parent().should(($div) => {
-                    const text = $div.text().replace(/\s\s+/g, ' ').trim();
-                    if (text) {
-                        const amt = text.substring(text.indexOf('$') + 1);
-                        if (amt) {
-                            const val = parseFloat(amt.substring(0, 4), 10) > 5;
-                            expect(val).to.be.true;
-                        }
-                    }
-                });
-            }
-        });
-    })
+describe('check fitness factory 0 results', () => {
+    const productIds = ['SPRJC','SPRDIP','SPRDCB','SPRCB','SPRBT','SPRBCB','SPRBACK','SPRACB','SPR500BACK','SPR500','SPR250','SPB368G','SP300','SPR1000BACK','SPR1000','DCLP-S','DCLPSF','DGYM','DABBSF','DABB-S','DBTCSF','DBTC-S','E5000','9PRECORAMT835-C','S2FT','LP40S','LO378','GSWT','GS348QP4','GPR400','BSTVD3','BSTND4','BSTND8','HAHTPH2','BSTFB6','HB223-LRG','GPCA1','GOBH5','GMRT6','GMR10-PACK','GLDA1','FSHDP','FSHDM','FMH','B4UB','LVSR','LVLA','LVBP','GLA348QS','GINV50','GFR500-PACK','GFR500','GFID225','GFID100','GFB350','GDR60UP','GDR60','GDR500-NEOPACK','GDR24-VPACK','GDCCRACK','GDCC250','GCRPACK','GCR100','GCA2','GBPR10','WSP200','VKR30','VDRA30-PACK','T150','T25','T50','GAR250','GAR100','FID46','G1S','G3S','G5S','G6BR','G10B','EXM4000S','EXM3000LPS','BSG10X','R300','PSS60X','PPRPS','PPR1000EXT','PPA13X','PGM200X','PFID130X','PFID125X','BSTND15','BSTSH','SDR12','SDR40','C2-SKIERG-STAND','C2-SKIERG','C2-BIKEERG','KBL15','KB75','KBX10KG','MB100A','MB022A','XTERRA2500','KBX32KG','KBV20','ST-6150-8RDE-10','ST-6160-8VS-LCD','ST-6160-8VS-2019-15-ATSC','ST-6160-8VS-10','ST-6150-8RDE-LCD','ST-6150-8RDE-2019-15-ATSC','ST-6140-8CT-LCD','ST-6140-8CT-2019-15-ATSC','ST-6140-8CT-10','RFRX2500T','ST-8TR-110-10','ST-8TR-110-19-ATSC','ST-8TR-110-LCD','ST-8TH-110-LCD','ST-8TRX-110-19-ATSC','ST-8TRX-110-10','ST-8TRX-110-LCDQ','ST-10TRX-110-LCDQ','ST-10TRX-110-19-ATSC'];
+    for (const product in productIds) {
+        it(`check for 0 results ${productIds[product]}`, () => {
+            cy.visit('https://dev.fitnessfactory.com');
+            cy.get('#headerSearch').type(productIds[product]);
+            cy.get('.btn.search').click();
+            cy.url().then(($url) => {
+                if ($url.includes('Search/index.cfm')) {
+                    cy.get('h1.category').eq(0).should(($div) => {
+                        const text = $div.text().replace(/\s\s+/g, ' ').trim();
+                        expect(text).not.to.contain('0 Results for');
+                    });
+                }
+            })
+        })
+    }
 })
