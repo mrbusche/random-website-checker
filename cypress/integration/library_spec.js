7b83d777314163d7eb279f2a4b70f559a3290a8a
Matt Busche <mrbusche@gmail.com>
2022-05-19 08:05:28 -0500

    Update library_spec.js

--- cypress/integration/library_spec.js
+++ cypress/integration/library_spec.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Blippo and Beep : I feel funny');
+        cy.get('.new-releases__link a').first().should('have.text', 'Loveboat, Taipei');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Uncharted');
+        cy.get('.new-releases__link a').first().should('have.text', 'Licorice pizza');
     })
 })
 
