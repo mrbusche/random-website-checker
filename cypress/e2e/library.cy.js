9bbef5fadec93f34fb5e9618b2927d0a758b6bbc
Matt Busche <mrbusche@gmail.com>
2022-06-07 08:56:50 -0500

    Update library.cy.js (#515)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'After the Romanovs : Russian exiles in Paris from the Belle Époque through revolution and war');
+        cy.get('.new-releases__link a').first().should('have.text', 'Armadillo antics');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '5 results');
+                cy.get('.search-text.h2').first().should('contain.text', '10 results');
             }
         });
     })
@@ -31,7 +31,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '9 results');
+                cy.get('.search-text.h2').first().should('contain.text', '14 results');
             }
         });
     })
