290b9b3ba54ae6cf16b937586fd6f68472f0a544
Matt Busche <mrbusche@gmail.com>
2022-07-09 14:23:26 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'So much more to Helen! : the passions and pursuits of Helen Keller');
+        cy.get('.new-releases__link a').first().should('have.text', 'No one is alone');
     })
 
     it('checks new videos', () => {
