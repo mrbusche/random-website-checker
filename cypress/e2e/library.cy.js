9ba74fde6d75901105592579986452a85a7d319d
Matt Busche <mrbusche@gmail.com>
2022-07-15 09:07:09 -0500

    Update library.cy.js (#560)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'How Maya got fierce');
+        cy.get('.new-releases__link a').first().should('have.text', 'The girls in Queens : a novel');
     })
 
     it('checks new videos', () => {
