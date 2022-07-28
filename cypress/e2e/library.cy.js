a0e8e8572a615730c7ca65712e8bd561d71f8e9c
Matt Busche <mrbusche@gmail.com>
2022-07-28 08:54:03 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Pup is lost');
+        cy.get('.new-releases__link a').first().should('have.text', 'Richard Scarry\'s busy busy airport');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Ends of the Earth');
+        cy.get('.new-releases__link a').first().should('have.text', 'Doctor Strange in the multiverse of madness');
     })
 })
 
