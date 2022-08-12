cd1d72b2dde4e36ab596db38212aa4df60e9fe7d
Matt Busche <mrbusche@gmail.com>
2022-08-12 08:56:07 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Biscuit and the great fall day');
+        cy.get('.new-releases__link a').first().should('have.text', 'D\'Aulaires' book of Greek myths');
     })
 
     it('checks new videos', () => {
