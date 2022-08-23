930e0d1eb6ccb42ff9cd26838add34b1942990b9
Matt Busche <mrbusche@gmail.com>
2022-08-23 09:31:31 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Korean celebrations : festivals, holidays and traditions');
+        cy.get('.new-releases__link a').first().should('have.text', 'Deadly spiritsDeadly spirits');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The mulligan');
+        cy.get('.new-releases__link a').first().should('have.text', 'The last waltz');
     })
 })
 
