f8c60a6a5097bb1d404f3e81488d03777a48909e
Matt Busche <mrbusche@gmail.com>
2022-08-25 08:59:53 -0500

    Update library.cy.js (#610)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Pinky and Rex and the spelling bee');
+        cy.get('.new-releases__link a').first().should('have.text', 'The Medici murders');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The last waltz');
+        cy.get('.new-releases__link a').first().should('have.text', 'My hero academia. World heroes\' mission');
     })
 })
 
