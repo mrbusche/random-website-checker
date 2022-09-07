6525e9d2a94baa64ce9e6cf09131d52dd4fb66b7
Matt Busche <mrbusche@gmail.com>
2022-09-07 07:37:54 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The devils of Loudun');
+        cy.get('.new-releases__link a').first().should('have.text', 'Fair is fair');
     })
 
     it('checks new videos', () => {
