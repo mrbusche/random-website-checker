66a13c6466fc3044d37f90cf8ae4f672cbad9a22
Matt Busche <mrbusche@gmail.com>
2022-09-22 10:59:39 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Lou');
+        cy.get('.new-releases__link a').first().should('have.text', 'People person');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Cobra Kai. Season 4');
+        cy.get('.new-releases__link a').first().should('have.text', 'Lightyear');
     })
 })
 
