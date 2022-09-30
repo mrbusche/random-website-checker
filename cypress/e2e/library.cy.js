4a05185f1545d59b72c060df3365a1ecbd7163a5
Matt Busche <mrbusche@gmail.com>
2022-09-30 11:54:05 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Dressing up the stars : the story of movie costume designer Edith Head');
+        cy.get('.new-releases__link a').first().should('have.text', 'Jaws');
     })
 
     it('checks new videos', () => {
