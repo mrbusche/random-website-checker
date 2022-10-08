1067018dc875eb1d5092ab51f5de96d1d4d63091
Matt Busche <mrbusche@gmail.com>
2022-10-08 16:07:05 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'From Saturday night to Sunday night : my forty years of laughter, tears, and touchdowns in TV');
+        cy.get('.new-releases__link a').first().should('have.text', 'Secrets of the Nile');
     })
 
     it('checks new videos', () => {
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '70 results');
+                cy.get('.search-text.h2').first().should('contain.text', '45 results');
             }
         });
     })
