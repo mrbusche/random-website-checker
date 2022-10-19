e3fa18e2ee930ca43706f2d4ca531724b15fdcff
Matt Busche <mrbusche@gmail.com>
2022-10-19 09:09:48 -0400

    Update library.cy.js (#672)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,13 +3,13 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 70');
+            expect(text).to.eq('1 - 10 of 72');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'What do we know about crop circles?');
+        cy.get('.new-releases__link a').first().should('have.text', 'The hike');
     })
 
     it('checks new videos', () => {
