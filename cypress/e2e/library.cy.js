1a0458ca0c83700f94dc8fef2d3c82add0edc985
Matt Busche <mrbusche@gmail.com>
2022-10-22 09:25:17 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Treasures of Egypt : a legacy in photographs from the pyramids to Cleopatra');
+        cy.get('.new-releases__link a').first().should('have.text', 'Furysong');
     })
 
     it('checks new videos', () => {
