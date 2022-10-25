f2868a555951e88ffd5da38ee86e973acf2fc752
Matt Busche <mrbusche@gmail.com>
2022-10-25 11:30:25 -0400

    Update library.cy.js (#681)
    * Update library.cy.js
    
    * Update CheckerApplicationTests.kt

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Furysong');
+        cy.get('.new-releases__link a').first().should('have.text', 'Rebels at sea : privateering in the American Revolution');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Rumble');
+        cy.get('.new-releases__link a').first().should('have.text', '2000 mules');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '9 results');
+                cy.get('.search-text.h2').first().should('contain.text', '8 results');
             }
         });
     })
