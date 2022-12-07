02addc67224e09c2e86cb1f2d3ac5d6497e99bdc
Matt Busche <mrbusche@gmail.com>
2022-12-07 10:29:59 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The diverticulitis handbook : how to live pain free');
+        cy.get('.new-releases__link a').first().should('have.text', 'Revenge of the librarians');
     })
 
     it('checks new videos', () => {
