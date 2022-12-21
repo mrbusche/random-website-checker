05e4fd0de77c7457b1e63c4a30acb674639e6638
Matt Busche <mrbusche@gmail.com>
2022-12-21 08:43:36 -0600

    Update library.cy.js (#768)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'A dash of salt and pepper');
+        cy.get('.new-releases__link a').first().should('have.text', 'The elements');
     })
 
     it('checks new videos', () => {
