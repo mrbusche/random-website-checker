84033e072b71e29d69e30ece4eed5fcd5f7ddc68
Matt Busche <mrbusche@gmail.com>
2022-12-23 13:37:51 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -14,7 +14,7 @@ describe('check kirkendall', () => {
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Ticket to paradise');
+        cy.get('.new-releases__link a').first().should('have.text', 'House of the dragon. 1');
     })
 })
 
