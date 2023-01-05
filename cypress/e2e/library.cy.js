18db7aaa1dd9869460886f7b2847be8ae4a28fb4
Matt Busche <mrbusche@gmail.com>
2023-01-05 08:56:12 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Construction site: you're just right : a valentine's lift-the-flap book');
+        cy.get('.new-releases__link a').first().should('have.text', 'Construction site: you\'re just right : a valentine's lift-the-flap book');
     })
 
     it('checks new videos', () => {
