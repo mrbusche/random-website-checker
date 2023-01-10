b22f5ea1c1f1a1a252e1138bff83d8ce2283806a
Matt Busche <mrbusche@gmail.com>
2023-01-10 07:38:25 -0600

    Update library.cy.js (#789)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'My mother\'s friend');
+        cy.get('.new-releases__link a').first().should('have.text', 'The sugar jar : create boundaries, embrace self-healing, and enjoy the sweet things in life');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Halloween ends');
+        cy.get('.new-releases__link a').first().should('have.text', 'The last man on Earth. The complete first season');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '28 results');
+                cy.get('.search-text.h2').first().should('contain.text', '32 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '31 results');
+                cy.get('.search-text.h2').first().should('contain.text', '34 results');
             }
         });
     })
