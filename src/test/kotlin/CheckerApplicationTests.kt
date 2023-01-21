2dc63b3ac5050acaf0e41decf2ac852bf30e0ea8
Matt Busche <mrbusche@gmail.com>
2023-01-20 21:20:08 -0600

    turn off new dev tracker

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -6,23 +6,23 @@ import org.junit.jupiter.api.Test
 
 internal class CheckerApplicationTests {
 
-    @Test
-    fun `Test New Development Counts`() {
-        // val document = Jsoup.connect("https://www.ankenyiowa.gov/our-city/maps/new-development").get()
-        val existingProjects = arrayListOf("2405 N Ankeny Blvd","704 SW Des Moines St","Ankeny Elementary #12","Ankeny Market Village","Ankeny Ortho","Aspen Ridge Foot/Ankle Clinic","Brakes Plus","Briarwood Plat 23, Lot 1","Brownstones at The District Phase 2","Canyon Landing Duplexes Plat 1","Canyon Landing Plat 2","Canyon Landing Townhomes Plat 1","Centennial Estates Plat 3","Centennial Ridge Plat 3","Cherry Glen Ballfield","Courtyards at Harvest Ridge Plat 4","Discount Tire","District 7 Parking Lot","District Building 5","Dunkin' Go","Fieldhouse","Four Mile Distribution Center South","Grove Landing Plat 5 &amp; 6","Home2.TRU Hotel","Housby","Hulsizer Commercial Lots 1 &amp; 2","I-35 Distribution Center","Keystone Church","Kimberley Estates Plat 2","Lot 1, Vintage Business Park at Prairie Trail Plat 2","Northgate Plat 6","Northview Lot 5","On With Life","Parkway Townhomes at Prairie Trail Plat 1","Popeye's Restaurant","RE/MAX Precision Sales Office","SW Des Moines Street Parks - Phase 1","Swanwood Industrial Park Plat 2, Lot 1","The Anker","The Crossings at Deer Creek Plat 12","The Reserve at the Grove","The Sterling North at Prairie Trail Plat 1","Vintage Business Park Plat 10")
-        assertFalse(CheckerService.retrieveNewProjects(existingProjects))
-    }
+//    @Test
+//    fun `Test New Development Counts`() {
+//        // val document = Jsoup.connect("https://www.ankenyiowa.gov/our-city/maps/new-development").get()
+//        val existingProjects = arrayListOf("2405 N Ankeny Blvd","704 SW Des Moines St","Ankeny Elementary #12","Ankeny Market Village","Ankeny Ortho","Aspen Ridge Foot/Ankle Clinic","Brakes Plus","Briarwood Plat 23, Lot 1","Brownstones at The District Phase 2","Canyon Landing Duplexes Plat 1","Canyon Landing Plat 2","Canyon Landing Townhomes Plat 1","Centennial Estates Plat 3","Centennial Ridge Plat 3","Cherry Glen Ballfield","Courtyards at Harvest Ridge Plat 4","Discount Tire","District 7 Parking Lot","District Building 5","Dunkin' Go","Fieldhouse","Four Mile Distribution Center South","Grove Landing Plat 5 &amp; 6","Home2.TRU Hotel","Housby","Hulsizer Commercial Lots 1 &amp; 2","I-35 Distribution Center","Keystone Church","Kimberley Estates Plat 2","Lot 1, Vintage Business Park at Prairie Trail Plat 2","Northgate Plat 6","Northview Lot 5","On With Life","Parkway Townhomes at Prairie Trail Plat 1","Popeye's Restaurant","RE/MAX Precision Sales Office","SW Des Moines Street Parks - Phase 1","Swanwood Industrial Park Plat 2, Lot 1","The Anker","The Crossings at Deer Creek Plat 12","The Reserve at the Grove","The Sterling North at Prairie Trail Plat 1","Vintage Business Park Plat 10")
+//        assertFalse(CheckerService.retrieveNewProjects(existingProjects))
+//    }
 
     @Test
     fun `Test Adoptium 11 Version`() {
         val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
-        assertEquals("jdk-11.0.17+8", CheckerService.retrieveSoftwareVersions(reportUrl, " Adoptium jdk11 linux x64"))
+        assertEquals("jdk-11.0.18+10", CheckerService.retrieveSoftwareVersions(reportUrl, " Adoptium jdk11 linux x64"))
     }
 
     @Test
     fun `Test Adoptium jdk17 Version`() {
         val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
-        assertEquals("jdk-17.0.5+8", CheckerService.retrieveSoftwareVersions(reportUrl, "Adoptium jdk17 linux x64"))
+        assertEquals("jdk-17.0.5+10", CheckerService.retrieveSoftwareVersions(reportUrl, "Adoptium jdk17 linux x64"))
     }
 
     @Test
