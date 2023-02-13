a2525369cec6eecfe31c22e7b2154e576789d5e2
Matt Busche <mrbusche@gmail.com>
2023-02-13 16:54:15 -0600

    Update dynasty.cy.js

--- cypress/e2e/dynasty.cy.js
+++ cypress/e2e/dynasty.cy.js
@@ -6,6 +6,6 @@ describe('check dynasty things', () => {
 
     it('checks ETR Dynasty updates', () => {
         cy.visit('https://establishtherun.com/etrs-dynasty-rankings/');
-        cy.get('span.published').first().should('have.text', 'May 4, 2022');
+        cy.get('span.published').first().should('have.text', 'Feb 13, 2023');
     })
 })
