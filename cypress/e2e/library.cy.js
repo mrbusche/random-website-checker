b7bcaa508da3cc4f5286cdc471bc77a169ec8c6e
Matt Busche <mrbusche@gmail.com>
2023-02-23 07:42:16 -0600

    Update library.cy.js (#845)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Ant-Man and Wasp save the day');
+        cy.get('.new-releases__link a').first().should('have.text', 'Stop, breathe, believe : mindful living one thought at a time');
     })
 
     it('checks new videos', () => {
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '32 results');
+                cy.get('.search-text.h2').first().should('contain.text', '39 results');
             }
         });
     })
