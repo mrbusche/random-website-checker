7794f3cc4547ce36e038014819031235ebc59ea1
Matt Busche <mrbusche@gmail.com>
2023-02-28 07:47:29 -0600

    Update library.cy.js (#855)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The menopause manifesto : own your health with facts and feminism');
+        cy.get('.new-releases__link a').first().should('have.text', 'Agents of S.U.I.T');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The last man on Earth. The complete third season');
+        cy.get('.new-releases__link a').first().should('have.text', 'Ronin.');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '30 results');
+                cy.get('.search-text.h2').first().should('contain.text', '29 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '35 results');
+                cy.get('.search-text.h2').first().should('contain.text', '32 results');
             }
         });
     })
