df4355d44eb4aa5a0a1fbf4609542a3bf73052c1
Matt Busche <mrbusche@gmail.com>
2023-03-03 07:14:45 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Roald Dahl's The witches : a set of plays');
+        cy.get('.new-releases__link a').first().should('have.text', 'Roald Dahl\'s The witches : a set of plays');
     })
 
     it('checks new videos', () => {
