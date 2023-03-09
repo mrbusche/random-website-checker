063d595453fea893f36009b8336b13f4958f2083
Matt Busche <mrbusche@gmail.com>
2023-03-09 08:02:28 -0600

    Update library.cy.js (#868)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Don\'t back down');
+        cy.get('.new-releases__link a').first().should('have.text', 'The moth keeper');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The old way');
+        cy.get('.new-releases__link a').first().should('have.text', 'Young Rock. Season one.');
     })
 })
 
