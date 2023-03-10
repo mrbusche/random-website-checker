2238c8e266b6e86b3e04adcd82cf7ba6c91fcce3
Matt Busche <mrbusche@gmail.com>
2023-03-10 09:17:04 -0600

    Update library.cy.js (#869)
    * Update library.cy.js
    
    * Update dynasty.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,18 +3,18 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 106');
+            expect(text).to.eq('1 - 10 of 95');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The moth keeper');
+        cy.get('.new-releases__link a').first().should('have.text', 'At the southern table with Paula Deen : 150 classic recipes to share with family and friends.');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Young Rock. Season one.');
+        cy.get('.new-releases__link a').first().should('have.text', 'Without a trace. The complete first season');
     })
 })
 
