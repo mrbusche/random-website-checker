41839eae02491b01aa86cefad3ef774f308139b8
Matt Busche <mrbusche@gmail.com>
2023-03-11 13:39:27 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'At the southern table with Paula Deen : 150 classic recipes to share with family and friends.');
+        cy.get('.new-releases__link a').first().should('have.text', 'Tote bags : sew 15 stunning projects and endless variations : includes 2 full-size reusable templates');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '64 results');
+                cy.get('.search-text.h2').first().should('contain.text', '63 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '97 results');
+                cy.get('.search-text.h2').first().should('contain.text', '92 results');
             }
         });
     })
