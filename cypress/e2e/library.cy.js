cab7883af102cb1e0116a45ef3798068847ed112
Matt Busche <mrbusche@gmail.com>
2023-04-18 10:21:39 -0500

    Update library.cy.js (#922)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -14,7 +14,7 @@ describe('check kirkendall', () => {
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Living');
+        cy.get('.new-releases__link a').first().should('have.text', 'Infinity pool');
     })
 })
 
