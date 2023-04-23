d7c6574ee8748e0ae5aa69b6a0e075ca6de2a222
Matt Busche <mrbusche@gmail.com>
2023-04-23 13:44:57 -0500

    Update library.cy.js (#929)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'What the Greeks did for us');
+        cy.get('.new-releases__link a').first().should('have.text', 'The song of wrath');
     })
 
     it('checks new videos', () => {
