a12a617b4f9ab86c44cab0ee533c591b3442d2bd
Matt Busche <mrbusche@gmail.com>
2023-04-29 12:08:11 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Ready to race! : Blaze and the Monster machines');
+        cy.get('.new-releases__link a').first().should('have.text', 'Ready to race! : The stopover');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '21 results');
+                cy.get('.search-text.h2').first().should('contain.text', '28 results');
             }
         });
     })
