aea3e2a9a082d6f7182c3c7a251b3d8227de4cb5
Matt Busche <mrbusche@gmail.com>
2023-05-02 12:25:13 -0500

    Update dynasty.cy.js

--- cypress/e2e/dynasty.cy.js
+++ cypress/e2e/dynasty.cy.js
@@ -1,7 +1,7 @@
 describe('check dynasty things', () => {
     it('checks ESPN Dynasty updates', () => {
         cy.visit('https://www.espn.com/fantasy/football/story/_/id/15698900/mike-clay-top-240-dynasty-fantasy-football-rankings-nfl');
-        cy.get('[data-dateformat="date1"]').first().should('have.text', 'Apr 24, 2023');
+        cy.get('[data-dateformat="date1"]').first().should('have.text', 'Apr 30, 2023');
     })
 
     it('checks ETR Dynasty updates', () => {
