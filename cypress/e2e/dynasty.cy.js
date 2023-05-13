d10b8bf5431be413cdb0425244ac54703fd0c7f9
Matt Busche <mrbusche@gmail.com>
2023-05-12 21:20:09 -0500

    Update library.cy.js (#955)
    * Update library.cy.js
    
    * Update dynasty.cy.js

--- cypress/e2e/dynasty.cy.js
+++ cypress/e2e/dynasty.cy.js
@@ -1,11 +1,11 @@
 describe('check dynasty things', () => {
-    it('checks ESPN Dynasty updates', () => {
-        cy.visit('https://www.espn.com/fantasy/football/story/_/id/15698900/mike-clay-top-240-dynasty-fantasy-football-rankings-nfl');
-        cy.get('[data-dateformat="date1"]').first().should('have.text', 'Apr 30, 2023');
-    })
+//     it('checks ESPN Dynasty updates', () => {
+//         cy.visit('https://www.espn.com/fantasy/football/story/_/id/15698900/mike-clay-top-240-dynasty-fantasy-football-rankings-nfl');
+//         cy.get('[data-dateformat="date1"]').first().should('have.text', 'Apr 30, 2023');
+//     })
 
     it('checks ETR Dynasty updates', () => {
         cy.visit('https://establishtherun.com/etrs-dynasty-rankings/');
-        cy.get('span.published').first().should('have.text', 'May 5, 2023');
+        cy.get('span.published').first().should('have.text', 'May 10, 2023');
     })
 })
