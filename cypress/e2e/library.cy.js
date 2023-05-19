0512161803b21d57cb2cd1ac418bfbd835d79a07
Matt Busche <mrbusche@gmail.com>
2023-05-18 21:33:43 -0500

    Update library.cy.js (#962)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Just Jerry : how drawing shaped my life : a memoir');
+        cy.get('.new-releases__link a').first().should('have.text', 'Mission at the museum');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'All quiet on the Western Front (Im Westen nichts Neues)');
+        cy.get('.new-releases__link a').first().should('have.text', 'Finding Forrester');
     })
 })
 
