d133cbe7280d91937ee4693080cfbe0a2ab471dd
Matt Busche <mrbusche@gmail.com>
2023-06-01 07:33:27 -0500

    Update library.cy.js (#979)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'A walk in the dark and other scary stories');
+        cy.get('.new-releases__link a').first().should('have.text', 'Party time, Crabby!');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Madame Blanc mysteries. Series 2');
+        cy.get('.new-releases__link a').first().should('have.text', 'The good fight. The final season');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '16 results');
+                cy.get('.search-text.h2').first().should('contain.text', '10 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '16 results');
+                cy.get('.search-text.h2').first().should('contain.text', '13 results');
             }
         });
     })
