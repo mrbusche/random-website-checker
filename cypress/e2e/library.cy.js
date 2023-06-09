6466ad5c066283792cca9fccb958a57ad9e8ba64
Matt Busche <mrbusche@gmail.com>
2023-06-09 08:58:28 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'They threw us away');
+        cy.get('.new-releases__link a').first().should('have.text', 'Marco Polo brave explorer');
     })
 
     it('checks new videos', () => {
