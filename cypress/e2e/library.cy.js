70bdda1df144dfed94b94e46d98476b904626241
Matt Busche <mrbusche@gmail.com>
2023-06-10 14:05:15 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Marco Polo brave explorer');
+        cy.get('.new-releases__link a').first().should('have.text', 'The viscount who vexed me');
     })
 
     it('checks new videos', () => {
