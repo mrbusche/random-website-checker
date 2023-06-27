d17442705f3a4325b3ebfa8133a807097ace61fb
Matt Busche <mrbusche@gmail.com>
2023-06-27 09:39:46 -0500

    Update library.cy.js (#1019)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'A day at the aquarium');
+        cy.get('.new-releases__link a').first().should('have.text', 'Bat-Boy Tim says boo!');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Arrested development : season four');
+        cy.get('.new-releases__link a').first().should('have.text', 'Guy Ritchie\'s The covenant');
     })
 })
 
