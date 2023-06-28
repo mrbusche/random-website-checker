034c8662b498357f4c14ddb3aa7a484fa3b14e6c
Matt Busche <mrbusche@gmail.com>
2023-06-28 13:13:30 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -14,7 +14,7 @@ describe('check kirkendall', () => {
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Looney Tunes Collector's Choice. vol. 1');
+        cy.get('.new-releases__link a').first().should('have.text', 'Looney Tunes Collector\'s Choice. vol. 1');
     })
 })
 
