71c5abf9d1354e08f637b567616c3d19d4a3127c
Matt Busche <mrbusche@gmail.com>
2023-07-06 07:56:37 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Ginny off the map');
+        cy.get('.new-releases__link a').first().should('have.text', 'Goblin mode : how to get cozy, embrace imperfection, and thrive in the muck');
     })
 
     it('checks new videos', () => {
