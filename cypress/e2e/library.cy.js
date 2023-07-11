2f517a43ce0a775dab0a211c95f17f60451ca3ab
Matt Busche <mrbusche@gmail.com>
2023-07-11 10:22:50 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The postcard');
+        cy.get('.new-releases__link a').first().should('have.text', 'Children\'s quick & easy cookbook');
     })
 
     it('checks new videos', () => {
