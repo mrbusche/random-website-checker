9bad4ca24449497857a2bdcd215bdba6b06b2806
Matt Busche <mrbusche@gmail.com>
2023-07-19 07:00:10 -0500

    Update library.cy.js (#1058)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'To dye for : how toxic fashion is making us sick--and how we can fight back');
+        cy.get('.new-releases__link a').first().should('have.text', 'The Collector : a novel');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Junk head');
+        cy.get('.new-releases__link a').first().should('have.text', 'Kill Her Goats');
     })
 })
 
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '115 results');
+                cy.get('.search-text.h2').first().should('contain.text', '139 results');
             }
         });
     })
