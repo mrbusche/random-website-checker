9b943ada30c7e1d7cf52e6bb9b55e7017d6326cd
Matt Busche <mrbusche@gmail.com>
2023-07-28 21:21:04 -0500

    daily update (#1071)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '61 results');
+                cy.get('.search-text.h2').first().should('contain.text', '71 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '68 results');
+                cy.get('.search-text.h2').first().should('contain.text', '78 results');
             }
         });
     })
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '9 results');
+                cy.get('.search-text.h2').first().should('contain.text', '4 results');
             }
         });
     })
