9bfeb2c8d24eb4ce225473fbf66c474cf586f763
Matt Busche <mrbusche@gmail.com>
2023-08-08 07:26:32 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Giant-sized butterflies on my first day of school');
+        cy.get('.new-releases__link a').first().should('have.text', 'Explore the beach');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '8 results');
+                cy.get('.search-text.h2').first().should('contain.text', '3 results');
             }
         });
     })
