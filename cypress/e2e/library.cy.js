a527628ef5e67229dfe3de8e0bd6d738dfa2b5d2
Matt Busche <mrbusche@gmail.com>
2023-08-17 09:28:10 -0500

    Update library.cy.js (#1097)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Body music : poems about the noises your body makes');
+        cy.get('.new-releases__link a').first().should('have.text', 'God is here');
     })
 
     it('checks new videos', () => {
