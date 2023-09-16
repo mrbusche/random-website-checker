621e8125105dfffaf18837d3be9e8f2360f58b96
Matt Busche <mrbusche@gmail.com>
2023-09-16 11:18:49 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Between monsters and marvels');
+        cy.get('.new-releases__link a').first().should('have.text', 'Giraffe math');
     })
 
     it('checks new videos', () => {
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '8 results');
+                cy.get('.search-text.h2').first().should('contain.text', '11 results');
             }
         });
     })
