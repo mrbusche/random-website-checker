49f9f3d98acb75bcaf6bc35d3fda4da17bbf71e7
Matt Busche <mrbusche@gmail.com>
2023-10-12 10:07:32 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Kiki kicks');
+        cy.get('.new-releases__link a').first().should('have.text', 'Harry at the dog show');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Sister Boniface mysteries. Season 2');
+        cy.get('.new-releases__link a').first().should('have.text', 'Transformers. Rise of the beasts');
     })
 })
 
