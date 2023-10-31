074ad43ffccf998a3ae14e84ae4703552af35e56
Matt Busche <mrbusche@gmail.com>
2023-10-31 08:19:47 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'What happens on vacation');
+        cy.get('.new-releases__link a').first().should('have.text', 'Lincoln : a photobiography');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Bonanza. The official twelfth season.');
+        cy.get('.new-releases__link a').first().should('have.text', 'The biscuit eater');
     })
 })
 
