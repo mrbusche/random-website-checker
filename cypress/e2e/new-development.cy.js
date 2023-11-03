8a7626dd23a131210d79cafe20c9d5cf2be82ece
Matt Busche <mrbusche@gmail.com>
2023-11-02 21:30:53 -0500

    fix for pop up (#1194)

--- cypress/e2e/new-development.cy.js
+++ cypress/e2e/new-development.cy.js
@@ -1,6 +1,7 @@
 describe('check new development', () => {
     it('checks for page size', () => {
         cy.visit('https://www.ankenyiowa.gov/Facilities');
+        cy.get('.cp-Splash-Btn.cp-Splash-Btn--NoShow').click();
         cy.get('#FacilityTypesButton').click();
         cy.get('#CheckAllFacilityTypes').click();
         cy.get('#chkSidebarFacilityTypes6').click();
