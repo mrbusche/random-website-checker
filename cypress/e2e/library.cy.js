0d30d292e19d79bb78084af81858546d23162fcd
Matt Busche <mrbusche@gmail.com>
2023-11-16 08:17:51 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'RAct like a lady, think like a lord');
+        cy.get('.new-releases__link a').first().should('have.text', 'Act like a lady, think like a lord');
     })
 
     it('checks new videos', () => {
