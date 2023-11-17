e50fa74d53bac8668f23dffbc12ed7abd519c6ce
Matt Busche <mrbusche@gmail.com>
2023-11-17 13:54:18 -0600

    Update library.cy.js (#1215)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Act like a lady, think like a lord');
+        cy.get('.new-releases__link a').first().should('have.text', 'The evolving truth of ever-stronger Will');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '44 results');
+                cy.get('.search-text.h2').first().should('contain.text', '15 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '33 results');
+                cy.get('.search-text.h2').first().should('contain.text', '12 results');
             }
         });
     })
