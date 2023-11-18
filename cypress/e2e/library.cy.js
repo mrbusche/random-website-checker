61e67d50aa5a260b12be214222cfab74de1aad3f
Matt Busche <mrbusche@gmail.com>
2023-11-18 09:21:35 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The evolving truth of ever-stronger Will');
+        cy.get('.new-releases__link a').first().should('have.text', 'Good girls don\'t die');
     })
 
     it('checks new videos', () => {
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '12 results');
+                cy.get('.search-text.h2').first().should('contain.text', '13 results');
             }
         });
     })
