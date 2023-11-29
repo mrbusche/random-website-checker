8d26bfef2e19f69e32ab7852ebe43df9edeea0ec
Matt Busche <mrbusche@gmail.com>
2023-11-29 17:21:57 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Robert B. Parker's broken trust');
+        cy.get('.new-releases__link a').first().should('have.text', 'Robert B. Parker\'s broken trust');
     })
 
     it('checks new videos', () => {
