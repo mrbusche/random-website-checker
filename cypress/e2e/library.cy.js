d32aaa9a6189f112b708212c0936352ae9576513
Matt Busche <mrbusche@gmail.com>
2024-01-20 12:36:05 -0600

    Update library.cy.js (#1291)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,13 +3,13 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 34');
+            expect(text).to.eq('1 - 10 of 35');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'We are the crisis');
+        cy.get('.new-releases__link a').first().should('have.text', 'The last fire season : a personal and pyronatural history');
     })
 
     it('checks new videos', () => {
