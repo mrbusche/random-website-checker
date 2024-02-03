76bc5693cd9df48bb7547113d50c65a198f4aa41
Matt Busche <mrbusche@gmail.com>
2024-02-03 14:14:54 -0600

    Update library.cy.js (#1307)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The dog across the lake');
+        cy.get('.new-releases__link a').first().should('have.text', 'Twenty thousand leagues under the sea');
     })
 
     it('checks new videos', () => {
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '16 results');
+                cy.get('.search-text.h2').first().should('contain.text', '15 results');
             }
         });
     })
