0f5a9c4dc21ed88f3271903904cf7a208f06aea7
Matt Busche <mrbusche@gmail.com>
2024-02-10 10:46:33 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Heart of the sun warrior');
+        cy.get('.new-releases__link a').first().should('have.text', '50 Oscar nights : iconic stars & filmmakers on their career-defining wins');
     })
 
     it('checks new videos', () => {
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '58 results');
+                cy.get('.search-text.h2').first().should('contain.text', '59 results');
             }
         });
     })
