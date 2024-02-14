fdb5a40ba1d6c8376ad25c93b8a59f0e31eb95d5
Matt Busche <mrbusche@gmail.com>
2024-02-14 07:34:42 -0600

    Update library.cy.js (#1317)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The antique hunter\'s guide to murder');
+        cy.get('.new-releases__link a').first().should('have.text', 'Dawn of the brave');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '11 results');
+                cy.get('.search-text.h2').first().should('contain.text', '57 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '15 results');
+                cy.get('.search-text.h2').first().should('contain.text', '62 results');
             }
         });
     })
