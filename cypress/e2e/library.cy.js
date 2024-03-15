4cbcee2db9f258aa9caf48ecf077601602dc0f18
Matt Busche <mrbusche@gmail.com>
2024-03-15 14:29:40 -0500

    Update library.cy.js (#1349)
    * Update library.cy.js
    
    * Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Daddy putdown.');
+        cy.get('.new-releases__link a').first().should('have.text', 'Home is the road : wandering the land, shaping the spirit');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '55 results');
+                cy.get('.search-text.h2').first().should('contain.text', '56 results');
             }
         });
     })
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '46 results');
+                cy.get('.search-text.h2').first().should('contain.text', '1 results');
             }
         });
     })
