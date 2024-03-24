a9f7ee165cbd9414fb419350de71f7b06d271dfd
Matt Busche <mrbusche@gmail.com>
2024-03-24 11:51:42 -0500

    remove tests until I want to learn more kotlin

--- src/test/kotlin/CheckerApplicationTests.kt
+++ src/test/kotlin/CheckerApplicationTests.kt
@@ -4,33 +4,33 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class CheckerApplicationTests {
-    @Test
-    fun `Test Adoptium 11 Version`() {
-        val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
-        assertEquals("jdk-11.0.22+7", CheckerService.retrieveSoftwareVersions(reportUrl, " Adoptium jdk11 linux x64"))
-    }
+    // @Test
+    // fun `Test Adoptium 11 Version`() {
+    //     val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
+    //     assertEquals("jdk-11.0.22+7", CheckerService.retrieveSoftwareVersions(reportUrl, " Adoptium jdk11 linux x64"))
+    // }
 
-    @Test
-    fun `Test Adoptium jdk17 Version`() {
-        val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
-        assertEquals("jdk-17.0.10+7", CheckerService.retrieveSoftwareVersions(reportUrl, "Adoptium jdk17 linux x64"))
-    }
+    // @Test
+    // fun `Test Adoptium jdk17 Version`() {
+    //     val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
+    //     assertEquals("jdk-17.0.10+7", CheckerService.retrieveSoftwareVersions(reportUrl, "Adoptium jdk17 linux x64"))
+    // }
 
-    @Test
-    fun `Test Adoptium jdk21 Version`() {
-        val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
-        assertEquals("jdk-21.0.2+13", CheckerService.retrieveSoftwareVersions(reportUrl, "Adoptium jdk21 linux x64"))
-    }
+    // @Test
+    // fun `Test Adoptium jdk21 Version`() {
+    //     val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
+    //     assertEquals("jdk-21.0.2+13", CheckerService.retrieveSoftwareVersions(reportUrl, "Adoptium jdk21 linux x64"))
+    // }
 
-    @Test
-    fun `Test Maven Version`() {
-        val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
-        assertEquals("4.0.0-alpha-13", CheckerService.retrieveSoftwareVersions(reportUrl, "Maven"))
-    }
+    // @Test
+    // fun `Test Maven Version`() {
+    //     val reportUrl = "https://github.com/jonathanlermitage/software-updates-bot/blob/master/report/report.md"
+    //     assertEquals("4.0.0-alpha-13", CheckerService.retrieveSoftwareVersions(reportUrl, "Maven"))
+    // }
 
-    @Test
-    fun `Test CentOS Version`() {
-        val reportUrl = "https://hub.docker.com/v2/repositories/library/centos/"
-        assertTrue(CheckerService.retrieveCentOSVersion(reportUrl, "[`centos7`, `7`, `centos7.9.2009`, `7.9.2009`]"))
-    }
+    // @Test
+    // fun `Test CentOS Version`() {
+    //     val reportUrl = "https://hub.docker.com/v2/repositories/library/centos/"
+    //     assertTrue(CheckerService.retrieveCentOSVersion(reportUrl, "[`centos7`, `7`, `centos7.9.2009`, `7.9.2009`]"))
+    // }
 }
