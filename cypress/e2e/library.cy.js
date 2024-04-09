e673dddcb4519033240f24aff7cc5242f0d77225
Matt Busche <mrbusche@gmail.com>
2024-04-09 16:53:57 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The exvangelicals : loving, living, and leaving the white evangelical church');
+        cy.get('.new-releases__link a').first().should('have.text', 'Wangari Maathai : the woman who planted millions of trees');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Dark winds. Season 1');
+        cy.get('.new-releases__link a').first().should('have.text', 'Ernest goes to camp ; Ernest scared stupid ; Ernest goes to jail');
     })
 })
 
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '46 results');
+                cy.get('.search-text.h2').first().should('contain.text', '40 results');
             }
         });
     })
