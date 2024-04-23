53ff5b1c23df5935068f03ad0627d3d2ecdf53fe
Matt Busche <mrbusche@gmail.com>
2024-04-23 09:51:39 -0500

    Update library.cy.js (#1395)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Native nations : a millennium in North America');
+        cy.get('.new-releases__link a').first().should('have.text', 'Shadows of the empire');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Dreamgirls');
+        cy.get('.new-releases__link a').first().should('have.text', 'The Swan Princess : mystery of the enchanted treasure');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '35 results');
+                cy.get('.search-text.h2').first().should('contain.text', '23 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '37 results');
+                cy.get('.search-text.h2').first().should('contain.text', '21 results');
             }
         });
     })
