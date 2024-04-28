c2cf68b99cde234ef01132c9941c835cac924807
Matt Busche <mrbusche@gmail.com>
2024-04-28 11:31:34 -0500

    Update library.cy.js (#1403)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Welcome to forever');
+        cy.get('.new-releases__link a').first().should('have.text', 'The chapter : a segmented history from antiquity to the twenty-first century');
     })
 
     it('checks new videos', () => {
