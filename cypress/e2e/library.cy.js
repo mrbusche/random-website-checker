b6d5fef6ebd98deabd6c705eb07d54ce9091b159
Matt Busche <mrbusche@gmail.com>
2024-05-16 08:01:02 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,18 +3,18 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 48');
+            expect(text).to.eq('1 - 10 of 34');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'For love of country : leave the Democrat Party behind');
+        cy.get('.new-releases__link a').first().should('have.text', 'Keeping 13');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Obi-Wan Kenobi : the complete series');
+        cy.get('.new-releases__link a').first().should('have.text', 'Because I said so');
     })
 })
 
