24b9c4054822f695d06f2ac40eed486322d89a86
Matt Busche <mrbusche@gmail.com>
2024-06-04 18:05:46 -0500

    Update library.cy.js (#1443)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,13 +3,13 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 43');
+            expect(text).to.eq('1 - 10 of 44');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Not your China doll : the wild and shimmering life of Anna May Wong');
+        cy.get('.new-releases__link a').first().should('have.text', 'Shut the door!');
     })
 
     it('checks new videos', () => {
