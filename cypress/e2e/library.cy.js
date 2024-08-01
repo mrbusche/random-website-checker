01785792452b5dc0861ec7f9ac7b2b0d81813aa5
Matt Busche <mrbusche@gmail.com>
2024-08-01 09:18:19 -0500

    Update library.cy.js (#1501)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Queen Esther saves her people');
+        cy.get('.new-releases__link a').first().should('have.text', 'Warrior king');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Back to black');
+        cy.get('.new-releases__link a').first().should('have.text', 'Hallmark 3-movie collection. A brush with love, When love springs, Wedding season.');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '47 results');
+                cy.get('.search-text.h2').first().should('contain.text', '45 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '51 results');
+                cy.get('.search-text.h2').first().should('contain.text', '60 results');
             }
         });
     })
