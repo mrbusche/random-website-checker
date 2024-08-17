232d70b27c731de0043b70e5369f609715118b40
Matt Busche <mrbusche@gmail.com>
2024-08-17 13:27:36 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Shameless : Republicans' deliberate dysfunction and the battle to preserve democracy');
+        cy.get('.new-releases__link a').first().should('have.text', 'Shameless : Republicans\' deliberate dysfunction and the battle to preserve democracy');
     })
 
     it('checks new videos', () => {
