0b7a8543a843d58e0c19684dd6d47a91417c18f0
Matt Busche <mrbusche@gmail.com>
2024-09-09 12:05:46 -0500

    Update library.cy.js (#1538)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Between the sound and sea : a novel');
+        cy.get('.new-releases__link a').first().should('have.text', 'The mystery of locked rooms');
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '7 results');
+                cy.get('.search-text.h2').first().should('contain.text', '10 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '12 results');
+                cy.get('.search-text.h2').first().should('contain.text', '16 results');
             }
         });
     })
