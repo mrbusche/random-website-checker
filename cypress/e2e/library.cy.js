fdcc2dcabd17b96ba1a075deab09b54b14f5d2fb
Matt Busche <mrbusche@gmail.com>
2024-11-07 10:24:02 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Woodsmoke women\'s book of spells');
+        cy.get('.new-releases__link a').first().should('have.text', 'The last close call');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The last Mimzy');
+        cy.get('.new-releases__link a').first().should('have.text', 'The Crow');
     })
 })
 
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '61 results');
+                cy.get('.search-text.h2').first().should('contain.text', '31 results');
             }
         });
     })
