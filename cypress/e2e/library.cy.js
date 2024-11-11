a5cd9a449073392fd30b81b4a67d997b128d6068
Matt Busche <mrbusche@gmail.com>
2024-11-11 09:39:03 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Trapped by tourism : sustainability questions for a world fueled by travelers');
+        cy.get('.new-releases__link a').first().should('have.text', 'The night before Christmas at Dunder Mifflin');
     })
 
     it('checks new videos', () => {
@@ -41,7 +41,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '31 results');
+                cy.get('.search-text.h2').first().should('contain.text', '0 results');
             }
         });
     })
