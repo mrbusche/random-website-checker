f7f4d5730cf379d5f82c8e8a437bd7ae8495fb0e
Matt Busche <mrbusche@gmail.com>
2024-11-14 15:07:04 -0600

    Update library.cy.js (#1609)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', 'Clever (and crafty) computers : enter a world of marvelous megabytes!');
+        cy.get('.new-releases__link a').first().should('have.text', 'Cloverton Charade');
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'Mayor of Kingstown. Season three.');
+        cy.get('.new-releases__link a').first().should('have.text', 'My life is murder. Series 4');
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '6 results');
+                cy.get('.search-text.h2').first().should('contain.text', '0 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '3 results');
+                cy.get('.search-text.h2').first().should('contain.text', '4 results');
             }
         });
     })
