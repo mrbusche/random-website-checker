122f4cd34c8826c68b81290b9dc2c8b6059da702
Matt Busche <mrbusche@gmail.com>
2024-12-12 11:41:05 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -14,7 +14,7 @@ describe('check kirkendall', () => {
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'White bird : a Wonder storyWhite bird : a Wonder story');
+        cy.get('.new-releases__link a').first().should('have.text', 'White bird : a Wonder story');
     })
 })
 
