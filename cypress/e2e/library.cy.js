b78432fd5d4c56edc1fd7d440addcf5c9d578a9c
Matt Busche <mrbusche@gmail.com>
2024-12-12 11:32:56 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,18 +3,18 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 57');
+            expect(text).to.eq('1 - 10 of 52');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "The dinosaur that pooped Christmas!");
+        cy.get('.new-releases__link a').first().should('have.text', "I spy love : a book of picture riddles");
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', 'The Mandalorian. The complete third season.');
+        cy.get('.new-releases__link a').first().should('have.text', 'White bird : a Wonder storyWhite bird : a Wonder story');
     })
 })
 
