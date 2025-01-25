f90381e09621efeda850619e30a5174031677042
Matt Busche <mrbusche@gmail.com>
2025-01-25 10:39:27 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "Presumed guilty");
+        cy.get('.new-releases__link a').first().should('have.text', "The Texas murders");
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '76 results');
+                cy.get('.search-text.h2').first().should('contain.text', '27 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '37 results');
+                cy.get('.search-text.h2').first().should('contain.text', '36 results');
                 
             }
         });
@@ -42,7 +42,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '2 results');
+                cy.get('.search-text.h2').first().should('contain.text', '0 results');
             }
         });
     })
