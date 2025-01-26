2659ba7536d2457764afdffe2163206e89a7eb87
Matt Busche <mrbusche@gmail.com>
2025-01-25 22:04:12 -0600

    add sivers book list

---
+++ cypress/e2e/sivers.js
@@ -0,0 +1,9 @@
+describe("check book prices", () => {
+  it("checks for first book", () => {
+    cy.visit("https://sive.rs/book?sort=date");
+    cy.get(".abook")
+      .first()
+      .should("have.attr", "data-date")
+      .and("equal", "2025-01-09");
+  });
+});
