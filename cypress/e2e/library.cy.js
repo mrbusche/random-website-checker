c3c9af2fc22f43c8f0a05dfedc050193f1aac171
Matt Busche <mrbusche@gmail.com>
2025-03-03 08:10:30 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "A forty year kiss");
+        cy.get('.new-releases__link a').first().should('have.text', "Visit Grand Canyon National Park!");
     })
 
     it('checks new videos', () => {
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '72 results');
+                cy.get('.search-text.h2').first().should('contain.text', '79 results');
                 
             }
         });
