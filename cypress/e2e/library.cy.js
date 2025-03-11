a904f27715e77fe1420ca7463700f98d03e25b6e
Matt Busche <mrbusche@gmail.com>
2025-03-11 16:50:01 -0500

    Update library.cy.js (#1716)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "Milly cow gives milk");
+        cy.get('.new-releases__link a').first().should('have.text', "Who is Tony Hawk?");
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', "Den of thieves 2 : Pantera");
+        cy.get('.new-releases__link a').first().should('have.text', "St. Elsewhere. Season one.");
     })
 })
 
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '4 results');
+                cy.get('.search-text.h2').first().should('contain.text', '6 results');
                 
             }
         });
