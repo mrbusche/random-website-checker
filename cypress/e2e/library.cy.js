00158666132d64bd8e6a5ff26170c047fecfbe2f
Matt Busche <mrbusche@gmail.com>
2025-03-24 10:22:43 -0500

    Update library.cy.js (#1725)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "How to handle cyberbullying");
+        cy.get('.new-releases__link a').first().should('have.text', "Thirty below : the harrowing and heroic story of the first all-women's ascent of Denali");
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '63 results');
+                cy.get('.search-text.h2').first().should('contain.text', '45 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h2').first().should('contain.text', '94 results');
+                cy.get('.search-text.h2').first().should('contain.text', '75 results');
                 
             }
         });
