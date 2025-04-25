6d7ebaa45ea8a0415ba89fe3410c3cdb56c5d102
Matt Busche <mrbusche@gmail.com>
2025-04-25 12:05:53 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "What's your dream? : find your passion, love your work, build a richer life");
+        cy.get('.new-releases__link a').first().should('have.text', "At school");
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '107 results');
+                cy.get('.search-text.h1').first().should('contain.text', '106 results');
             }
         });
     })
