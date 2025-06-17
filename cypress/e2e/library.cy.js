c9bfc81d8c5842f2764c921493e1e925925ee71b
Matt Busche <mrbusche@gmail.com>
2025-06-17 12:24:19 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "Murder takes a vacation");
+        cy.get('.new-releases__link a').first().should('have.text', "Iris and Walter and the field trip");
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', "The friend");
+        cy.get('.new-releases__link a').first().should('have.text', "Babylon Berlin. Season 4");
     })
 })
 
