444f7f44c5d72cf115ce04194cec4186736595a2
Matt Busche <mrbusche@gmail.com>
2025-06-18 15:57:11 -0500

    Update library.cy.js (#1793)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,7 +9,7 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "Iris and Walter and the field trip");
+        cy.get('.new-releases__link a').first().should('have.text', "The naked consumer : how our private lives become public commodities");
     })
 
     it('checks new videos', () => {
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '19 results');
+                cy.get('.search-text.h1').first().should('contain.text', '31 results');
 
             }
         });
