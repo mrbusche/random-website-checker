0b8dc19b32d236419e10a090ec847f348cc81436
Matt Busche <mrbusche@gmail.com>
2025-06-24 15:51:03 -0500

    Update library.cy.js (#1797)
    * Update library.cy.js
    
    * Update sivers.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,13 +3,13 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 47');
+            expect(text).to.eq('1 - 10 of 43');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "The day the crayons made friends");
+        cy.get('.new-releases__link a').first().should('have.text', "Really bird, really good beach party!");
     })
 
     it('checks new videos', () => {
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '7 results');
+                cy.get('.search-text.h1').first().should('contain.text', '8 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '23 results');
+                cy.get('.search-text.h1').first().should('contain.text', '26 results');
 
             }
         });
