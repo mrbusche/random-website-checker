6c1edc7cf3dc2c48f5e778167dd6eabebb9a8dee
Matt Busche <mrbusche@gmail.com>
2025-06-25 11:03:51 -0500

    Update library.cy.js (#1798)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "Really bird, really good beach party!");
+        cy.get('.new-releases__link a').first().should('have.text', "The president's shadow");
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', "Babylon Berlin. Season 4");
+        cy.get('.new-releases__link a').first().should('have.text', "A Minecraft movie");
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '8 results');
+                cy.get('.search-text.h1').first().should('contain.text', '9 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '26 results');
+                cy.get('.search-text.h1').first().should('contain.text', '17 results');
 
             }
         });
