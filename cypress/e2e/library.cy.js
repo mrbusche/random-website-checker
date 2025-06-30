bc639182f47851a44664604ba10f686f5463a7cb
Matt Busche <mrbusche@gmail.com>
2025-06-30 11:55:40 -0500

    Update library.cy.js (#1802)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,13 +3,13 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 48');
+            expect(text).to.eq('1 - 10 of 49');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "Geraldine, the music mouse");
+        cy.get('.new-releases__link a').first().should('have.text', "No new things : a radically simple 30-day guide to saving money, the planet, and your sanity");
     })
 
     it('checks new videos', () => {
