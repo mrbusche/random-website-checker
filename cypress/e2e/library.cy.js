a4c8e87c07f4257881dfd7d4820bfc36b0eb9806
Matt Busche <mrbusche@gmail.com>
2025-07-24 10:12:49 -0500

    Update library.cy.js (#1826)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -9,12 +9,12 @@ describe('check kirkendall', () => {
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "¡Estamos en un libro!");
+        cy.get('.new-releases__link a').first().should('have.text', "Naturally : the herbalist's guide to health and transformation");
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', "Fuller house. The fifth and final season");
+        cy.get('.new-releases__link a').first().should('have.text', "Ash");
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '3 results');
+                cy.get('.search-text.h1').first().should('contain.text', '28 results');
             }
         });
     })
@@ -32,7 +32,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '9 results');
+                cy.get('.search-text.h1').first().should('contain.text', '28 results');
 
             }
         });
