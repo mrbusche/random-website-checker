d0aa0e1643b32a1051bc82f8e0becffc0ed1e0a9
Matt Busche <mrbusche@gmail.com>
2025-08-09 21:55:03 -0400

    Update library.cy.js (#1838)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -3,18 +3,18 @@ describe('check kirkendall', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/search/searchresults.aspx?ctx=3.1033.0.0.5&type=Keyword&term=%22:%20ON%20ORDER%22&by=KW&sort=RELEVANCE&limit=TOM=*&query=&page=0&searchid=1');
         cy.get('.c-results-utility-result-count').eq(1).should(($div) => {
             const text = $div.text().replace(/\s\s+/g, ' ').trim();
-            expect(text).to.eq('1 - 10 of 59');
+            expect(text).to.eq('1 - 10 of 62');
         });
     })
 
     it('checks new books', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-        cy.get('.new-releases__link a').first().should('have.text', "People like us : or The other continent, or Johnny Wordcount stumbles into a high-end croissant bar on the Seine in search of The Kid & orders the big dream");
+        cy.get('.new-releases__link a').first().should('have.text', "We should all be birds : (a memoir)");
     })
 
     it('checks new videos', () => {
         cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-        cy.get('.new-releases__link a').first().should('have.text', "All of my heart : inn love");
+        cy.get('.new-releases__link a').first().should('have.text', "Scarecrow and Mrs. King. The complete first season");
     })
 })
 
@@ -23,7 +23,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '33 results');
+                cy.get('.search-text.h1').first().should('contain.text', '43 results');
             }
         });
     })
@@ -42,7 +42,7 @@ describe('check bridges', () => {
         cy.visit('https://bridges.overdrive.com/bridges-kirkendall/magazines/collection/217420?addedDate=days-0-7&sortBy=newlyadded&language=en');
         cy.get('body').then((body) => {
             if (!body.find('.Results-noResultsHeading').length > 0) {
-                cy.get('.search-text.h1').first().should('contain.text', '0 results');
+                cy.get('.search-text.h1').first().should('contain.text', '77 results');
             }
         });
     })
