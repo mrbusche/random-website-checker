b0af8f81f51623b4d22b3caa6e9262394431af54
Matt Busche <mrbusche@gmail.com>
2025-08-17 14:27:29 -0500

    log values

--- cypress/e2e/flight-checker.cy.js
+++ cypress/e2e/flight-checker.cy.js
@@ -1,5 +1,5 @@
-describe('check skate clinics', () => {
-  it('checks for new clinics', () => {
+describe('check flight prices', () => {
+  it('checks flight prices', () => {
     cy.visit(
       'https://www.google.com/travel/flights?tfs=CA0QAhpiEgoyMDI2LTAxLTE0IiAKA0RTTRIKMjAyNi0wMS0xNBoDT1JEKgJBQTIENDEzNiIgCgNPUkQSCjIwMjYtMDEtMTQaA1BVSioCQUEyBDI2MTNqBwgBEgNEU01yBwgBEgNQVUoaYhIKMjAyNi0wMS0yMCIgCgNQVUoSCjIwMjYtMDEtMjAaA0NMVCoCQUEyBDMwNDEiIAoDQ0xUEgoyMDI2LTAxLTIwGgNEU00qAkFBMgQxMzI1agcIARIDUFVKcgcIARIDRFNNQAJAAkABQAFIAVIDVVNEemhDalJJVVd0UVRraDBPWEpFY0hkQlFub3RjMUZDUnkwdExTMHRMUzB0YjJ0aWFYSXpPRUZCUVVGQlIyaHRVV3BKU3pabVNVRkJFZ0V3R2dzSTNPTU5FQUlhQTFWVFJEZ1pjTnpqRFE9PcgBAA&curr=USD&utm_source=FLRE&utm_medium=EMAIL&utm_campaign=PC&utm_content=FLIGHT',
     );
@@ -8,6 +8,8 @@ describe('check skate clinics', () => {
       .then((text) => {
         const numbersOnly = text.replace(/[^0-9]/g, '');
         const value = parseInt(numbersOnly, 10);
+        cy.log(`numbersOnly: ${numbersOnly}`);
+        cy.log(`value: ${value}`);
         expect(value).to.be.at.least(3400);
       });
   });
