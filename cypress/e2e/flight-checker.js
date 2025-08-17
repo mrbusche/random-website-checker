59fd65153cb1a8c730fc49da9770d1c91338ec89
Matt Busche <mrbusche@gmail.com>
2025-08-17 14:20:37 -0500

    add flight checker

---
+++ cypress/e2e/flight-checker.js
@@ -0,0 +1,14 @@
+describe('check skate clinics', () => {
+  it('checks for new clinics', () => {
+    cy.visit(
+      'https://www.google.com/travel/flights?tfs=CA0QAhpiEgoyMDI2LTAxLTE0IiAKA0RTTRIKMjAyNi0wMS0xNBoDT1JEKgJBQTIENDEzNiIgCgNPUkQSCjIwMjYtMDEtMTQaA1BVSioCQUEyBDI2MTNqBwgBEgNEU01yBwgBEgNQVUoaYhIKMjAyNi0wMS0yMCIgCgNQVUoSCjIwMjYtMDEtMjAaA0NMVCoCQUEyBDMwNDEiIAoDQ0xUEgoyMDI2LTAxLTIwGgNEU00qAkFBMgQxMzI1agcIARIDUFVKcgcIARIDRFNNQAJAAkABQAFIAVIDVVNEemhDalJJVVd0UVRraDBPWEpFY0hkQlFub3RjMUZDUnkwdExTMHRMUzB0YjJ0aWFYSXpPRUZCUVVGQlIyaHRVV3BKU3pabVNVRkJFZ0V3R2dzSTNPTU5FQUlhQTFWVFJEZ1pjTnpqRFE9PcgBAA&curr=USD&utm_source=FLRE&utm_medium=EMAIL&utm_campaign=PC&utm_content=FLIGHT',
+    );
+    cy.get('span.tZe0ff')
+      .invoke('text')
+      .then((text) => {
+        const numbersOnly = text.replace(/[^0-9]/g, '');
+        const value = parseInt(numbersOnly, 10);
+        expect(value).to.be.at.least(3000);
+      });
+  });
+});
