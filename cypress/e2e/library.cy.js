f58d7b26c40f43b2d425dfccb9bc0f4cc0a4c3e4
Matt Busche <mrbusche@gmail.com>
2025-08-19 08:54:31 -0500

    Update library.cy.js (#1848)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -13,12 +13,12 @@ describe('check kirkendall', () => {
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', 'Elvis & Romeo go to dog school');
+    cy.get('.new-releases__link a').first().should('have.text', 'Fabulous fountains of New York');
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', 'The accountant 2');
+    cy.get('.new-releases__link a').first().should('have.text', 'Friendship');
   });
 });
 
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '6 results');
+        cy.get('.search-text.h1').first().should('contain.text', '4 results');
       }
     });
   });
@@ -47,7 +47,7 @@ describe('check bridges', () => {
     );
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '0 results');
+        cy.get('.search-text.h1').first().should('contain.text', '3 results');
       }
     });
   });
