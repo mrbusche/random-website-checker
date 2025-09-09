14da3a43c615210bf669787c4d9db8ffe30ea4a0
Matt Busche <mrbusche@gmail.com>
2025-09-09 15:46:43 -0500

    Update library.cy.js (#1865)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,13 +7,13 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 61');
+        expect(text).to.eq('1 - 10 of 71');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Hot desk");
+    cy.get('.new-releases__link a').first().should('have.text', "Schattenfroh");
   });
 
   it('checks new videos', () => {
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '62 results');
+        cy.get('.search-text.h1').first().should('contain.text', '63 results');
       }
     });
   });
