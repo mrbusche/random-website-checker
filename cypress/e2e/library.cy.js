e8f2a3d9438b99b98d5030b40da411f46d4b724f
Matt Busche <mrbusche@gmail.com>
2025-09-11 12:15:54 -0500

    Update library.cy.js (#1867)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,18 +7,18 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 71');
+        expect(text).to.eq('1 - 10 of 57');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Schattenfroh");
+    cy.get('.new-releases__link a').first().should('have.text', "Frizzy, curly, swirly hair! : a children's book about being unique");
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', "Experiment in terror");
+    cy.get('.new-releases__link a').first().should('have.text', "Ballerina");
   });
 });
 
@@ -27,7 +27,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '70 results');
+        cy.get('.search-text.h1').first().should('contain.text', '13 results');
       }
     });
   });
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '63 results');
+        cy.get('.search-text.h1').first().should('contain.text', '7 results');
       }
     });
   });
@@ -47,7 +47,7 @@ describe('check bridges', () => {
     );
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '0 results');
+        cy.get('.search-text.h1').first().should('contain.text', '46 results');
       }
     });
   });
