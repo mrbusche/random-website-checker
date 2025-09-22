6cd5253b840bb7ff635e0620bf20135b88be533c
Matt Busche <mrbusche@gmail.com>
2025-09-21 21:00:03 -0500

    new book

--- cypress/e2e/sivers.cy.js
+++ cypress/e2e/sivers.cy.js
@@ -4,6 +4,6 @@ describe("check book prices", () => {
     cy.get(".abook")
       .first()
       .should("have.attr", "data-date")
-      .and("equal", "2025-08-18");
+      .and("equal", "2025-09-14");
   });
 });
