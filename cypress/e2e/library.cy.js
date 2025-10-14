89fe8e98bd908331c4b40bf7af9e5600e4c5534d
Matt Busche <mrbusche@gmail.com>
2025-10-14 08:39:41 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -13,7 +13,7 @@ describe('check kirkendall', () => {
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Wild card");
+    cy.get('.new-releases__link a').first().should('have.text', "Take a walk with the wind");
   });
 
   it('checks new videos', () => {
@@ -27,7 +27,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '24 results');
+        cy.get('.search-text.h1').first().should('contain.text', '9 results');
       }
     });
   });
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '38 results');
+        cy.get('.search-text.h1').first().should('contain.text', '11 results');
       }
     });
   });
@@ -47,7 +47,7 @@ describe('check bridges', () => {
     );
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '1 results');
+        cy.get('.search-text.h1').first().should('contain.text', '3 results');
       }
     });
   });
