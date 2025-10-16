5733d6dbe79427b5f072585c2140423099c240d4
Matt Busche <mrbusche@gmail.com>
2025-10-16 11:39:56 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,18 +7,18 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 66');
+        expect(text).to.eq('1 - 10 of 58');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Chocolat : Parisian desserts and other delights");
+    cy.get('.new-releases__link a').first().should('have.text', "My life as a coder");
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', "The Bad Guys 2");
+    cy.get('.new-releases__link a').first().should('have.text', "The Fantastic 4 : First steps");
   });
 });
 
@@ -47,7 +47,7 @@ describe('check bridges', () => {
     );
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '3 results');
+        cy.get('.search-text.h1').first().should('contain.text', '2 results');
       }
     });
   });
