a61d934828c38b9c963cf1e4e439d7441a70c4fa
Matt Busche <mrbusche@gmail.com>
2025-10-23 10:58:46 -0500

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -13,12 +13,12 @@ describe('check kirkendall', () => {
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Wagon train");
+    cy.get('.new-releases__link a').first().should('have.text', "Mr. Putter & Tabby bake the cake");
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', "Weapons");
+    cy.get('.new-releases__link a').first().should('have.text', "Hamish MacBeth. Series two");
   });
 });
 
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '53 results');
+        cy.get('.search-text.h1').first().should('contain.text', '54 results');
       }
     });
   });
@@ -47,7 +47,7 @@ describe('check bridges', () => {
     );
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '3 results');
+        cy.get('.search-text.h1').first().should('contain.text', '8 results');
       }
     });
   });
