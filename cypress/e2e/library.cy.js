d7801b1982c62ce299a7833eeaf7856ce894ddb3
Matt Busche <mrbusche@gmail.com>
2025-12-15 07:28:41 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,13 +7,13 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 49');
+        expect(text).to.eq('1 - 10 of 55');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Nightmare obscura : a dream engineer's guide through the sleeping mind");
+    cy.get('.new-releases__link a').first().should('have.text', "Quantum Tempest");
   });
 
   it('checks new videos', () => {
@@ -27,7 +27,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '50 results');
+        cy.get('.search-text.h1').first().should('contain.text', '0 results');
       }
     });
   });
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '51 results');
+        cy.get('.search-text.h1').first().should('contain.text', '9 results');
       }
     });
   });
