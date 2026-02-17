daea6a869130b156cc42e8ebb2b33e1e0dd6d9b2
Matt Busche <mrbusche@gmail.com>
2026-02-17 11:32:19 -0600

    Update library.cy.js (#1948)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,13 +7,13 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 73');
+        expect(text).to.eq('1 - 10 of 76');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Beinoni");
+    cy.get('.new-releases__link a').first().should('have.text', "Troubled waters : a river's journey toward justice");
   });
 
   it('checks new videos', () => {
@@ -27,7 +27,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '59 results');
+        cy.get('.search-text.h1').first().should('contain.text', '56 results');
       }
     });
   });
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '90 results');
+        cy.get('.search-text.h1').first().should('contain.text', '102 results');
       }
     });
   });
