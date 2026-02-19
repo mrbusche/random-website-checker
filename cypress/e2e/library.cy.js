ab128131a066a8c33d774d210597747b7bd70a4f
Matt Busche <mrbusche@gmail.com>
2026-02-19 10:36:41 -0600

    Update library.cy.js

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,18 +7,18 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 76');
+        expect(text).to.eq('1 - 10 of 58');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "The intimate animal : the science of sex, fidelity, and why we live and die for love");
+    cy.get('.new-releases__link a').first().should('have.text', "Baked to perfection : delicious gluten-free recipes with a pinch of science");
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', "Nuremberg");
+    cy.get('.new-releases__link a').first().should('have.text', "Keeper");
   });
 });
 
