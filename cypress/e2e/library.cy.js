829a6904729c4e0ca3756c2c91bf41adb512c8ca
Matt Busche <mrbusche@gmail.com>
2026-02-26 20:52:47 -0600

    use rolling tags

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,13 +7,13 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 65');
+        expect(text).to.eq('1 - 10 of 64');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Here come the aunties!");
+    cy.get('.new-releases__link a').first().should('have.text', "Eowulf : the creature connection");
   });
 
   it('checks new videos', () => {
