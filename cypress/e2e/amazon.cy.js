cfd080b17c37055fa7677776fab8c033894b9cfb
Matt Busche <mrbusche@gmail.com>
2026-03-07 16:53:05 -0600

    Handle potential continue shopping button (#1964)
    Add conditional click for first button if present

--- cypress/e2e/amazon.cy.js
+++ cypress/e2e/amazon.cy.js
@@ -3,6 +3,13 @@ describe("check book prices", () => {
     cy.visit(
       "https://www.amazon.com/hz/wishlist/ls/153OV2P85MJD6?type=wishlist&filter=unpurchased&sort=price-asc&viewType=list"
     );
+
+    cy.get("body").then(($body) => {
+      if ($body.find(".a-button-text").length) {
+        cy.get(".a-button-text").first().click({ force: true });
+      }
+    });
+
     cy.get(".g-item-sortable")
       .first()
       .should("have.attr", "data-price")
