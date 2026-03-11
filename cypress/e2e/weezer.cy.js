c428a043d33d232fd11731b4470ba674f50f81d1
Matt Busche <mrbusche@gmail.com>
2026-03-10 20:20:16 -0500

    check weezer (#1967)
    * check weezer
    
    * we got line breaks

---
+++ cypress/e2e/weezer.cy.js
@@ -0,0 +1,8 @@
+describe("check book prices", () => {
+  it("checks for first book", () => {
+    cy.visit("https://weezerwebstore.com/products/weezer-coloring-book-flipbook");
+    cy.get(".badge.price__badge-sold-out.color-scheme-3")
+      .first()
+      .should('contain.text', 'Sold out');
+  });
+});
