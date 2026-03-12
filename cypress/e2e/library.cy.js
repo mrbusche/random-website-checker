4f1c039feaa36d79f2fe58728ef25b7e7354be7f
Matt Busche <mrbusche@gmail.com>
2026-03-12 11:00:40 -0500

    Update library.cy.js (#1969)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -7,18 +7,18 @@ describe('check kirkendall', () => {
       .eq(1)
       .should(($div) => {
         const text = $div.text().replace(/\s\s+/g, ' ').trim();
-        expect(text).to.eq('1 - 10 of 56');
+        expect(text).to.eq('1 - 10 of 55');
       });
   });
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "The unhurried homeschooler : a simple, mercifully short book on homeschooling");
+    cy.get('.new-releases__link a').first().should('have.text', "Son of the demon");
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', "Heroes : the complete series");
+    cy.get('.new-releases__link a').first().should('have.text', "Young man with ideas");
   });
 });
 
