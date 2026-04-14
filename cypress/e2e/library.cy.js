c63ff2e1ee4b65a073d8a023175ffda335eadb25
Matt Busche <mrbusche@gmail.com>
2026-04-14 08:10:52 -0400

    Update expected text for new books and results (#1984)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -13,12 +13,12 @@ describe('check kirkendall', () => {
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Two kinds of stranger");
+    cy.get('.new-releases__link a').first().should('have.text', "Trad wife");
   });
 
   it('checks new videos', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    cy.get('.new-releases__link a').first().should('have.text', "Good luck Charlie : Enjoy the ride.");
+    cy.get('.new-releases__link a').first().should('have.text', "The knick. The complete first season");
   });
 });
 
@@ -27,7 +27,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '35 results');
+        cy.get('.search-text.h1').first().should('contain.text', '31 results');
       }
     });
   });
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '44 results');
+        cy.get('.search-text.h1').first().should('contain.text', '43 results');
       }
     });
   });
