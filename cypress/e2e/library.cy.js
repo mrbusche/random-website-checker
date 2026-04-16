ab9842503d40f2a4bdaa0f2ea09acd09475ccb1c
Matt Busche <mrbusche@gmail.com>
2026-04-16 15:35:49 -0500

    Update library.cy.js (#1986)

--- cypress/e2e/library.cy.js
+++ cypress/e2e/library.cy.js
@@ -13,7 +13,7 @@ describe('check kirkendall', () => {
 
   it('checks new books', () => {
     cy.visit('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    cy.get('.new-releases__link a').first().should('have.text', "Chocolate baking : the ultimate guide to cakes, cookies, desserts and pastries");
+    cy.get('.new-releases__link a').first().should('have.text', "Small-girl Toni and the quest for gold");
   });
 
   it('checks new videos', () => {
@@ -27,7 +27,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37479?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '20 results');
+        cy.get('.search-text.h1').first().should('contain.text', '10 results');
       }
     });
   });
@@ -36,7 +36,7 @@ describe('check bridges', () => {
     cy.visit('https://bridges.overdrive.com/bridges-kirkendall/content/collection/37473?addedDate=days-0-7');
     cy.get('body').then((body) => {
       if (!body.find('.Results-noResultsHeading').length > 0) {
-        cy.get('.search-text.h1').first().should('contain.text', '31 results');
+        cy.get('.search-text.h1').first().should('contain.text', '21 results');
       }
     });
   });
