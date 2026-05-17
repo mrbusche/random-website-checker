264fcfcc0d3399ada37480d627b60f13c6b38a89
Matt Busche <mrbusche@gmail.com>
2026-05-16 21:19:05 -0400

    upgrade everything (#2017)

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -10,11 +10,6 @@ test.describe('check kirkendall', () => {
     expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 52');
   });
 
-  test('checks new books', async ({ page }) => {
-    await page.goto('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=26');
-    await expect(page.locator('.new-releases__link a').first()).toHaveText('Home no matter where');
-  });
-
   test('checks new videos', async ({ page }) => {
     await page.goto('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
     await expect(page.locator('.new-releases__link a').first()).toHaveText('Top Gun');
@@ -27,7 +22,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('4 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('5 results');
     }
   });
 
@@ -36,7 +31,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('23 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('22 results');
     }
   });
 });
