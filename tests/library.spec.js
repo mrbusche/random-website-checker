b6e5eb63d02b5a0cb2237bb6563f3e13ff382842
Matt Busche <mrbusche@gmail.com>
2026-06-10 11:11:46 -0500

    Update library.spec.js (#2030)

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -7,12 +7,12 @@ test.describe('check kirkendall', () => {
     );
 
     const text = await page.locator('.c-results-utility-result-count').nth(1).innerText();
-    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 52');
+    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 49');
   });
 
   test('checks new videos', async ({ page }) => {
     await page.goto('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    await expect(page.locator('.new-releases__link a').first()).toHaveText('Peter Pan');
+    await expect(page.locator('.new-releases__link a').first()).toHaveText('Lorne');
   });
 });
 
