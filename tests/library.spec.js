8bab31d27f39b7a525ede1a68f1761b8d606ef7e
Matt Busche <mrbusche@gmail.com>
2026-07-15 14:35:27 -0500

    Update expected result counts in library tests

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -7,12 +7,12 @@ test.describe('check kirkendall', () => {
     );
 
     const text = await page.locator('.c-results-utility-result-count').nth(1).innerText();
-    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 29');
+    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 19');
   });
 
   test('checks new videos', async ({ page }) => {
     await page.goto('https://kplcatalog.ankenyiowa.gov/polaris/Search/newreleases.aspx?ListingTypeID=27');
-    await expect(page.locator('.new-releases__link a').first()).toHaveText('Strange journey : the story of Rocky Horror');
+    await expect(page.locator('.new-releases__link a').first()).toHaveText('Obsession');
   });
 });
 
@@ -22,7 +22,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('47 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('42 results');
     }
   });
 
@@ -31,7 +31,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('57 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('35 results');
     }
   });
 });
