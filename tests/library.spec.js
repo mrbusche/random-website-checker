f7a48d3a52a23cfde1097b88a647d940d05c51f4
Matt Busche <mrbusche@gmail.com>
2026-07-20 15:51:01 -0500

    Mrbusche patch 1 (#2054)
    * Update library.spec.js
    
    * Update expected results in library tests

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -7,7 +7,7 @@ test.describe('check kirkendall', () => {
     );
 
     const text = await page.locator('.c-results-utility-result-count').nth(1).innerText();
-    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 19');
+    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 35');
   });
 
   test('checks new videos', async ({ page }) => {
@@ -22,7 +22,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('42 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('0 results');
     }
   });
 
@@ -31,7 +31,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('34 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('9 results');
     }
   });
 });
