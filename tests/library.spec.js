cf78d56641c2a726d0be2bcca00524d456571e52
Matt Busche <mrbusche@gmail.com>
2026-07-20 15:07:29 -0500

    Update expected results in library tests

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -22,7 +22,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('42 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('0 results');
     }
   });
 
@@ -31,7 +31,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('34 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('9 results');
     }
   });
 });
