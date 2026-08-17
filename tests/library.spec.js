5a646b55588e2269ead534e2a26983270073c618
Matt Busche <mrbusche@gmail.com>
2026-08-17 14:40:05 -0500

    Update library.spec.js

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -7,7 +7,7 @@ test.describe('check kirkendall', () => {
     );
 
     const text = await page.locator('.c-results-utility-result-count').nth(1).innerText();
-    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 44');
+    expect(text.replace(/\s\s+/g, ' ').trim()).toBe('1 - 10 of 48');
   });
 
   test('checks new videos', async ({ page }) => {
@@ -22,7 +22,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('30 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('23 results');
     }
   });
 
@@ -31,7 +31,7 @@ test.describe('check bridges', () => {
 
     const hasNoResults = (await page.locator('.Results-noResultsHeading').count()) > 0;
     if (!hasNoResults) {
-      await expect(page.locator('.search-text.h1').first()).toContainText('177 results');
+      await expect(page.locator('.search-text.h1').first()).toContainText('167 results');
     }
   });
 });
